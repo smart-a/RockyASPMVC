4e7322c9d2e10dcf1a54e0c51846faabe719dca1
smart-a <olalekan.adesoye@gmail.com>
2021-10-28 12:22:52 +0100

    Add Application Type Model and migration

---
+++ RockyASPMVC/Models/ApplicationType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RockyASPMVC.Models
+{
+    public class ApplicationType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
